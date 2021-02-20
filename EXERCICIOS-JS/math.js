7e5769967f121edb21c5c2ca83a6f04bd39caa1d
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-02-20 18:03:18 -0300

    Entendenco os numeros, math e algumas curiosidades em javascript

---
+++ EXERCICIOS-JS/math.js
@@ -0,0 +1,7 @@
+//Math no Javascript
+
+const raio = 5.6
+const area = Math.PI * Math.pow(raio,2)
+
+console.log(area);
+console.log(typeof Math); // Retorna o tipo Objeto
---
+++ EXERCICIOS-JS/numerosAlgunsCuidados.js
@@ -0,0 +1,8 @@
+//Curiosidade do Javascript
+
+console.log(7/0);       // Valor infinito(Infinity)
+console.log("10" / 2);  //Divisão de uma String em javascript
+console.log("Show!"*2); //NaN - Not a Number
+console.log(0.1 + 0.7); //Impressição numerica
+//console.log(10.toString()); //Não é possivel executar
+console.log((10.345).toFixed(4));
