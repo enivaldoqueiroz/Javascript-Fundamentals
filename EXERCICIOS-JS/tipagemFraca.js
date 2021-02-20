eed3655e8a480afe49a9c2a04b88394e54400a28
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-02-20 17:42:36 -0300

    Entendo a criação de variaveis, constantes e tipagem fraca em javascript

---
+++ EXERCICIOS-JS/tipagemFraca.js
@@ -0,0 +1,12 @@
+// Tipagem no Javascript
+
+let qualquer = 'Legal'
+
+qualquer = 3,1516
+console.log(qualquer);
+console.log(typeof qualquer);
+
+// Devemos evitar nomes genericos e siglas
+let valor = ''
+let numero = 1
+let pqp = false // pqp - Profduto Quimico Perigoso... kkk
---
+++ EXERCICIOS-JS/variaveis_e_constantes.js
@@ -0,0 +1,17 @@
+// Declaração de Variaveis e Constantes
+var a = 3
+let b = 4
+
+var a = 30
+b = 40
+console.log(a,b);
+
+a = 300
+b = 400
+
+console.log(a,b);
+
+//Declaraçã de Constate no javascript
+const c = 5
+//c = 50
+console.log(c);
