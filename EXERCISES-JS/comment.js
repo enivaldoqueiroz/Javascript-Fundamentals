e74bd5de2bc125c820fa46d3aba6fd5c70b6ec18
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-02-20 15:43:38 -0300

    Primeiro commit - Organização basica de um codigo JS e Comentarios no codigo

---
+++ EXERCISES-JS/comment.js
@@ -0,0 +1,15 @@
+// Comentario de uma linha
+console.log('Linha 1');
+
+/**
+ * Comentarios de 
+ * múltiplas linhas
+ */
+console.log('Linha 2');
+
+/** 
+ * >3 Dicas
+ * 1: Evitar comentarios Óbvios no codigo;
+ * 2: Codigo claro sem muitos comentarios e
+ * 3: Comentar o contesto do codigo.
+ */
---
+++ EXERCISES-JS/organization.js
@@ -0,0 +1,17 @@
+/**
+ * >>ORGANIZAÇÃO DO JAVASCRIPT
+ * O Javascript é organizado em sentença de codido ou bloco de codigo.
+ * Com ; ou não.
+ */
+
+//Sentença de codigo
+console.log("Sentença de código");
+
+//Bloco de codigo
+{
+    {
+        console.log("Olá");
+        console.log("Mundo") //Pradrão do curso
+    }
+}
+
