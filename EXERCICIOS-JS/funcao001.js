2f8b065090124ee36c005d2c3113a0e571fca34c
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-02-28 18:55:48 -0300

    Entendendo os exemplos de funções em Javascript

---
+++ EXERCICIOS-JS/funcao001.js
@@ -0,0 +1,19 @@
+// Funções sem retorno
+
+function imprimirSoma(a,b){
+    console.log(a + b);
+}
+
+imprimirSoma(2, 3)  // Saida 5
+imprimirSoma(2)     // Saida NaN
+imprimirSoma(2, 10, 4, 5, 6, 7, 8)// Saida 5
+imprimirSoma()      // Saida NaN
+
+
+// Funções sem retorno
+function soma(a , b = 1){
+    return a + b
+}
+
+console.log(soma(2, 3));
+console.log(soma(2));
---
+++ EXERCICIOS-JS/funcaoEmQuaseTudo.js
@@ -0,0 +1,4 @@
+console.log(typeof Object); // O Object é uma função em Javascript
+
+class Produto {}
+    console.log(typeof Produto); // Produto também é uma função em Javascript
