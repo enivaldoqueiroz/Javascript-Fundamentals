634a0f960c7b3cb8569ca0d720e2ef8760f791b6
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-02-28 21:40:31 -0300

    Conceito de Hosting usando var e let - Içamento de uma variavel para o top

---
+++ EXERCICIOS-JS/hosting.js
@@ -0,0 +1,23 @@
+// Conceito de Hosting usando var = Içamento da variavel para o top
+
+// console.log('a =', a);
+
+// var a = 2
+
+// console.log('a =', a);
+
+/*function teste(){
+    console.log('a =', a);
+    var a = 2
+    console.log('a =', a);
+}
+
+teste()
+console.log(teste);
+*/
+
+// Conceito de Hosting usando let = Içamento da variavel para o top
+
+console.log('b =',a);
+let b = 2
+console.log('b =',a);
