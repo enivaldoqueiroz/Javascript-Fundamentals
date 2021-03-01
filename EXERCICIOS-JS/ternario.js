e5fedddf4faa40b18ea7b552b3c39ea5fc33b2e5
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-03-01 00:36:28 -0300

    Operador Unario e Ternario

---
+++ EXERCICIOS-JS/ternario.js
@@ -0,0 +1,8 @@
+// Operador Ternario
+//Expressão ? Expressão verdadeira : Expressão falsa
+const resultado = nota => nota >= 7 ? 'Aprovado': 'Reprovado'
+console.log(resultado(7.1));
+console.log(resultado(6.7));
+
+
+
