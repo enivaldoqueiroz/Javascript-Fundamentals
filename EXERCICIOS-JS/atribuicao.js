3b8b6b22e3e84160a660d272c7af1211e8d59475
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-02-28 22:36:24 -0300

    Operadores de Atribuição

---
+++ EXERCICIOS-JS/atribuicao.js
@@ -0,0 +1,17 @@
+// Operadores de Atribuição 
+const a = 7
+let b = 3
+
+b += a //b = b + a - Atribuição aditiva
+console.log(b);
+
+b -= 4 //b = b - a - Atribuição subtrativa
+console.log(b);
+
+b *= 2 //b = b * a - Atribuição multiplicativa
+console.log(b);
+
+b /= 2 //b = b / a - Atribuição divisiva
+console.log(b);
+b %= 2 //b = b - a - Atribuição modular
+console.log(b);
