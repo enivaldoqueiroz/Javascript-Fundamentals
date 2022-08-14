0fd8ba758f304e99ef20514f3ca1ef584bc42f8a
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2022-08-14 18:31:08 -0300

    Fundamentos no Javascript

---
+++ FUNDAMENTOS/comentarios.js
@@ -0,0 +1,15 @@
+// Comentario de uma linha
+
+console.log('Linha 1');
+
+/*
+    Cometarios de
+    multiplas linhas
+*/
+console.log('Linha 2');
+
+/**
+ * Comentarios de 
+ * multiplas linha
+ */
+console.log('Linha 3');
